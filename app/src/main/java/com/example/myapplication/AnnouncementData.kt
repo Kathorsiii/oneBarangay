dbf6dfc188579cd4830460c032baf822c7d26bdd
Katherine Mariñas <katherinemarinas11@gmail.com>
2021-11-18 17:54:16 +0800

    announcement

--- app/src/main/java/com/example/myapplication/AnnouncementData.kt
+++ app/src/main/java/com/example/myapplication/AnnouncementData.kt
@@ -1,8 +1,11 @@
 package com.example.myapplication
 
+import java.sql.Timestamp
+
 data class AnnouncementData(
     // var announcement_id: String ?= null,
     var title: String ?= null,
     var body: String ?= null,
+//    var creation_date: Timestamp ?= null,
     var thumbnail: String ?= null,
 )
