5182b5d08e4abb17906d7988018ee1269b3f7b04
PrynsTag <princevelasco16@gmail.com>
2021-11-18 17:06:59 +0800

    refactorp[imports]: remove fragment imports

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,16 +3,6 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.fragments.DashboardFragment
-import com.example.myapplication.fragments.NotificationsFragment
-import com.example.myapplication.fragments.ProfileFragment
-import com.example.myapplication.fragments.ServicesFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.firestore.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
