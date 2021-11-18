2a62d7af72aa85957fff79e1d29f67bc5cd39eb2
PrynsTag <princevelasco16@gmail.com>
2021-11-19 01:15:21 +0800

    feat[appointment]: implement view appointment

--- app/src/main/java/com/example/myapplication/AppointmentDisplayActivity.kt
+++ app/src/main/java/com/example/myapplication/AppointmentDisplayActivity.kt
@@ -1,19 +1,22 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityAppointmentDisplayBinding
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_appointment_display.*
 
 class AppointmentDisplayActivity : AppCompatActivity() {
 
     // ActionBar
     private lateinit var actionBar: ActionBar
+    private lateinit var binding: ActivityAppointmentDisplayBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_appointment_display)
+        binding = ActivityAppointmentDisplayBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // Actionbar
         actionBar = supportActionBar!!
@@ -22,20 +25,25 @@ class AppointmentDisplayActivity : AppCompatActivity() {
         // Back button
         actionBar.setDisplayHomeAsUpEnabled(true)
 
-        val  = intent.getStringExtra("")
-        val  = intent.getStringExtra("")
-        val  = intent.getStringExtra("")
-        val appointmentImgURL = intent.getStringArrayExtra("appointment_image")
+        val firstName = intent.getStringExtra("first_name")
+        val lastName = intent.getStringExtra("last_name")
+        val appointmentImage = intent.getStringExtra("appointment_image")
+        val appointmentPurpose = intent.getStringExtra("appointment_purpose")
+        val startAppointment = intent.getStringExtra("start_appointment")
+        val endAppointment = intent.getStringExtra("end_appointment")
+        val status = intent.getStringExtra("status")
+        val documents = intent.getStringExtra("documents")
 
-        appointmentAccountNameText.text
-        appointmentAccountTypeText.text
-        appointmentStartDateText.text
-        appointmentEndDateText.text
-        documentTypeText.text
-        documentPurposeText.text
+        binding.appointmentAccountNameText.text = "$firstName $lastName"
+        binding.appointmentDocumentStatus.text = status
+        binding.documentTypeText.text = documents
+        binding.documentPurposeText.text = appointmentPurpose
+        binding.appointmentStartDateText.text = startAppointment
+        binding.appointmentEndDateText.text = endAppointment
 
-        Picasso.get().load(appointmentImgURL).into(appointmentImage)
-
+        Picasso.get()
+            .load(appointmentImage)
+            .into(binding.appointmentImage)
     }
 
     override fun onSupportNavigateUp(): Boolean {
