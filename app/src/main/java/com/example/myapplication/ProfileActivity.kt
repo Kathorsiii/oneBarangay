7d250f1048709957d76900f7213fe2b952b651dd
Katherine Mariñas <katherinemarinas11@gmail.com>
2021-11-18 23:54:12 +0800

    ProfileActivity

--- app/src/main/java/com/example/myapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/ProfileActivity.kt
@@ -45,81 +45,6 @@ class ProfileActivity : AppCompatActivity() {
         // Init FirebaseAuth
         firebaseAuth = FirebaseAuth.getInstance()
 
-        val db = Firebase.firestore
-
-        val userID = firebaseAuth.uid!!
-
-        // println(userID)
-
-        // For viewing
-        val viewUsers = db.collection("users").document(userID)
-        viewUsers.get()
-            .addOnSuccessListener { document ->
-                if (document != null) {
-
-                    var firstName = document.getString("first_name")
-                    var middleName = document.getString("middle_name")
-                    var lastName = document.getString("last_name")
-
-                    var fullName = "$firstName $middleName $lastName"
-
-                    val imageProfileURL = document.getString("photo_url")
-
-                    val profileImageView = findViewById<ImageView>(R.id.userProfileImage)
-
-                    Picasso.get().load(imageProfileURL).into(profileImageView)
-
-                    binding.nameUserProfile.setText(fullName)
-                    binding.idTextInput.setText(document.getString("user_id"))
-                    binding.roleUserInput.setText(document.getString("role"))
-                    binding.emailAddressUser.setText(document.getString("email"))
-                    binding.addressUserInput.setText(document.getString("address"))
-                    binding.streetUserInput.setText(document.getString("street"))
-                    binding.contactNumberUser.setText(document.getString("contact_number"))
-//                    binding.ageUserInput.setText(document.getString("age"))
-                    binding.birthPlaceUser.setText(document.getString("birth_place"))
-                    binding.citizenshipUser.setText(document.getString("citizenship"))
-                    binding.civilStatusUser.setText(document.getString("civil_status"))
-                    binding.dateOfBirthUser.setText(document.getString("date_of_birth"))
-                    binding.monthlyIncomeUser.setText(document.getString("monthly_income"))
-
-                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                } else {
-                    Log.d(TAG, "No such document")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
-
-        // ====================================================== //
-
-        binding.updateUserBtn.setOnClickListener {
-            // For Update
-            val updateUsers = db.collection("users").document(userID)
-                .update(mapOf(
-                    "address" to binding.addressUserInput.text.toString(),
-                    "contact_number" to binding.contactNumberUser.text.toString()
-                ))
-                .addOnSuccessListener {
-                    Log.d(TAG, "DocumentSnapshot successfully updated!")
-                    val view = View.inflate(this@ProfileActivity, R.layout.success_dialog_update_user, null)
-                    val builder = AlertDialog.Builder(this@ProfileActivity)
-                    builder.setView(view)
-
-                    val dialog = builder.create()
-                    dialog.show()
-
-                    view.okay_update_btn.setOnClickListener {
-                        dialog.dismiss()
-                        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-                    }
-                }
-                .addOnFailureListener {
-                        e -> Log.w(TAG, "Error updating document", e)
-                }
-        }
-
         checkUser()
 
         // Handle click logout
@@ -135,6 +60,80 @@ class ProfileActivity : AppCompatActivity() {
 
         if (firebaseUser != null) {
             // If user is not null, user is logged in, get the user info
+            val db = Firebase.firestore
+
+            val userID = firebaseAuth.uid!!
+
+            // println(userID)
+
+            // For viewing
+            val viewUsers = db.collection("users").document(userID)
+            viewUsers.get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+
+                        var firstName = document.getString("first_name")
+                        var middleName = document.getString("middle_name")
+                        var lastName = document.getString("last_name")
+
+                        var fullName = "$firstName $middleName $lastName"
+
+                        val imageProfileURL = document.getString("photo_url")
+
+                        val profileImageView = findViewById<ImageView>(R.id.userProfileImage)
+
+                        Picasso.get().load(imageProfileURL).into(profileImageView)
+
+                        binding.nameUserProfile.setText(fullName)
+                        binding.idTextInput.setText(document.getString("user_id"))
+                        binding.roleUserInput.setText(document.getString("role"))
+                        binding.emailAddressUser.setText(document.getString("email"))
+                        binding.addressUserInput.setText(document.getString("address"))
+                        binding.streetUserInput.setText(document.getString("street"))
+                        binding.contactNumberUser.setText(document.getString("contact_number"))
+//                    binding.ageUserInput.setText(document.getString("age"))
+                        binding.birthPlaceUser.setText(document.getString("birth_place"))
+                        binding.citizenshipUser.setText(document.getString("citizenship"))
+                        binding.civilStatusUser.setText(document.getString("civil_status"))
+                        binding.dateOfBirthUser.setText(document.getString("date_of_birth"))
+                        binding.monthlyIncomeUser.setText(document.getString("monthly_income"))
+
+                        Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                    } else {
+                        Log.d(TAG, "No such document")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "get failed with ", exception)
+                }
+
+            // ====================================================== //
+
+            binding.updateUserBtn.setOnClickListener {
+                // For Update
+                val updateUsers = db.collection("users").document(userID)
+                    .update(mapOf(
+                        "address" to binding.addressUserInput.text.toString(),
+                        "contact_number" to binding.contactNumberUser.text.toString()
+                    ))
+                    .addOnSuccessListener {
+                        Log.d(TAG, "DocumentSnapshot successfully updated!")
+                        val view = View.inflate(this@ProfileActivity, R.layout.success_dialog_update_user, null)
+                        val builder = AlertDialog.Builder(this@ProfileActivity)
+                        builder.setView(view)
+
+                        val dialog = builder.create()
+                        dialog.show()
+
+                        view.okay_update_btn.setOnClickListener {
+                            dialog.dismiss()
+                            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+                        }
+                    }
+                    .addOnFailureListener {
+                            e -> Log.w(TAG, "Error updating document", e)
+                    }
+            }
 
         } else {
             // If user is null, user if not logged in, go to Login Activity
