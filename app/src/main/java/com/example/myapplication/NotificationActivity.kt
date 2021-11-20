e275f65841a73c551ea460f0a7013afc4920ddc6
Katherine Mariñas <katherinemarinas11@gmail.com>
2021-11-20 16:27:52 +0800

    init[notification]

--- app/src/main/java/com/example/myapplication/NotificationActivity.kt
+++ app/src/main/java/com/example/myapplication/NotificationActivity.kt
@@ -1,8 +1,13 @@
 package com.example.myapplication
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class NotificationActivity : AppCompatActivity() {
 
@@ -19,6 +24,21 @@ class NotificationActivity : AppCompatActivity() {
 
         // Back button
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
     }
 
     override fun onSupportNavigateUp(): Boolean {
